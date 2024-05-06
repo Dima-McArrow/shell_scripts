a33fea86904e0b532075e142311ccb17c410faf8
Dima-McArrow <dsm.box@gmail.com>
2024-05-06 21:29:16 +0200

    more changes

--- test.sh
+++ test.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/zsh
 
+# Use gum to choose the branch
+selected_branch=$(git branch | gum choose | sed 's/^\* //;s/^[ \t]*//;s/[ \t]*$//')
 
-git branch | gum choose | xargs git checkout
+# Get the current branch name
+current_branch=$(git branch --show-current)
 
+# Check if the selected branch is the same as the current branch
+if [[ "$selected_branch" == "$current_branch" ]]; then
+  echo "You are already on '$current_branch'. No need to switch branches."
+else
+  # Checkout to the selected branch if different
+  git checkout "$selected_branch"
+fi
